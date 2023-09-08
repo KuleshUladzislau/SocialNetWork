5567e61e992bf1eccc79b5304bbd3960c4fedd89
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-08 12:26:29 +0300

    hashrouter test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "app/store/store";
 import App from "app/App";
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
 );
 
 
