de3faeb7f56345391e3665a38e750f8cc56af077
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-08 12:43:50 +0300

    added based url

--- src/index.tsx
+++ src/index.tsx
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
 );
 
 
